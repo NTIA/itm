da340d935ef45a3bb7205d35103a6678360fc7fa
Anthony Romaniello <aromaniello@ntia.gov>
2024-08-27 16:38:58 -0400

    Use cross platform "EXPORTED" instead of "DLLEXPORT"

--- include/ITS.Propagation.ITM/ITM.h
+++ include/ITS.Propagation.ITM/ITM.h
@@ -13,8 +13,15 @@ namespace ITM {
 
 using namespace std;
 
-// Export the DLL functions as "C" and not C++
-#define DLLEXPORT extern "C" __declspec(dllexport)
+// Define cross-platform EXPORTED
+#ifndef DOXYGEN_SHOULD_SKIP
+    #ifdef _WIN32
+        #define EXPORTED extern "C" __declspec(dllexport)
+    #else
+        #define EXPORTED extern "C"
+    #endif
+#endif
+
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 #define DIM(x, y) (((x) > (y)) ? (x - y) : (0))
@@ -48,67 +55,67 @@ struct IntermediateValues
 /////////////////////////////
 // Main ITM Functions
 
-DLLEXPORT int ITM_P2P_TLS(double h_tx__meter, double h_rx__meter, double pfl[], int climate, double N_0, double f__mhz,
+EXPORTED int ITM_P2P_TLS(double h_tx__meter, double h_rx__meter, double pfl[], int climate, double N_0, double f__mhz,
     int pol, double epsilon, double sigma, int mdvar, double time, double location, double situation,
     double *A__db, long *warnings);
-DLLEXPORT int ITM_P2P_TLS_Ex(double h_tx__meter, double h_rx__meter, double pfl[], int climate, double N_0, double f__mhz,
+EXPORTED int ITM_P2P_TLS_Ex(double h_tx__meter, double h_rx__meter, double pfl[], int climate, double N_0, double f__mhz,
     int pol, double epsilon, double sigma, int mdvar, double time, double location, double situation,
     double *A__db, long *warnings, IntermediateValues *interValues);
-DLLEXPORT int ITM_P2P_CR(double h_tx__meter, double h_rx__meter, double pfl[], int climate, double N_0, double f__mhz,
+EXPORTED int ITM_P2P_CR(double h_tx__meter, double h_rx__meter, double pfl[], int climate, double N_0, double f__mhz,
     int pol, double epsilon, double sigma, int mdvar, double confidence, double reliability,
     double *A__db, long *warnings);
-DLLEXPORT int ITM_P2P_CR_Ex(double h_tx__meter, double h_rx__meter, double pfl[], int climate, double N_0, double f__mhz,
+EXPORTED int ITM_P2P_CR_Ex(double h_tx__meter, double h_rx__meter, double pfl[], int climate, double N_0, double f__mhz,
     int pol, double epsilon, double sigma, int mdvar, double confidence, double reliability,
     double *A__db, long *warnings, IntermediateValues *interValues);
-DLLEXPORT int ITM_AREA_TLS(double h_tx__meter, double h_rx__meter, int tx_site_criteria, int rx_site_criteria, double d__km,
+EXPORTED int ITM_AREA_TLS(double h_tx__meter, double h_rx__meter, int tx_site_criteria, int rx_site_criteria, double d__km,
     double delta_h__meter, int climate, double N_0, double f__mhz, int pol, double epsilon, double sigma,
     int mdvar, double time, double location, double situation, double *A__db, long *warnings);
-DLLEXPORT int ITM_AREA_TLS_Ex(double h_tx__meter, double h_rx__meter, int tx_site_criteria, int rx_site_criteria, double d__km,
+EXPORTED int ITM_AREA_TLS_Ex(double h_tx__meter, double h_rx__meter, int tx_site_criteria, int rx_site_criteria, double d__km,
     double delta_h__meter, int climate, double N_0, double f__mhz, int pol, double epsilon, double sigma,
     int mdvar, double time, double location, double situation, double *A__db, long *warnings, IntermediateValues *interValues);
-DLLEXPORT int ITM_AREA_CR(double h_tx__meter, double h_rx__meter, int tx_site_criteria, int rx_site_criteria, double d__km,
+EXPORTED int ITM_AREA_CR(double h_tx__meter, double h_rx__meter, int tx_site_criteria, int rx_site_criteria, double d__km,
     double delta_h__meter, int climate, double N_0, double f__mhz, int pol, double epsilon, double sigma,
     int mdvar, double confidence, double reliability, double *A__db, long *warnings);
-DLLEXPORT int ITM_AREA_CR_Ex(double h_tx__meter, double h_rx__meter, int tx_site_criteria, int rx_site_criteria, double d__km,
+EXPORTED int ITM_AREA_CR_Ex(double h_tx__meter, double h_rx__meter, int tx_site_criteria, int rx_site_criteria, double d__km,
     double delta_h__meter, int climate, double N_0, double f__mhz, int pol, double epsilon, double sigma,
     int mdvar, double confidence, double reliability, double *A__db, long *warnings, IntermediateValues *interValues);
 
 /////////////////////////////
 // ITM Helper Functions
 
-DLLEXPORT double ComputeDeltaH(double pfl[], double d_start__meter, double d_end__meter);
-DLLEXPORT double DiffractionLoss(double d__meter, double d_hzn__meter[2], double h_e__meter[2], complex<double> Z_g,
+EXPORTED double ComputeDeltaH(double pfl[], double d_start__meter, double d_end__meter);
+EXPORTED double DiffractionLoss(double d__meter, double d_hzn__meter[2], double h_e__meter[2], complex<double> Z_g,
     double a_e__meter, double delta_h__meter, double h__meter[2], int mode, double theta_los, double d_sML__meter, double f__mhz);
-DLLEXPORT double FFunction(double td);
-DLLEXPORT void FindHorizons(double pfl[], double a_e__meter, double h__meter[2], double theta_hzn[2], double d_hzn__meter[2]);
-DLLEXPORT double FreeSpaceLoss(double d__meter, double f__mhz);
-DLLEXPORT double FresnelIntegral(double v2);
-DLLEXPORT double H0Function(double r, double eta_s);
-DLLEXPORT double HeightFunction(double x__km, double K);
-DLLEXPORT void InitializeArea(int site_criteria[2], double gamma_e, double delta_h__meter,
+EXPORTED double FFunction(double td);
+EXPORTED void FindHorizons(double pfl[], double a_e__meter, double h__meter[2], double theta_hzn[2], double d_hzn__meter[2]);
+EXPORTED double FreeSpaceLoss(double d__meter, double f__mhz);
+EXPORTED double FresnelIntegral(double v2);
+EXPORTED double H0Function(double r, double eta_s);
+EXPORTED double HeightFunction(double x__km, double K);
+EXPORTED void InitializeArea(int site_criteria[2], double gamma_e, double delta_h__meter,
     double h__meter[2], double h_e__meter[2], double d_hzn__meter[2], double theta_hzn[2]);
-DLLEXPORT void InitializePointToPoint(double f__mhz, double h_sys__meter, double N_0, int pol, double epsilon, 
+EXPORTED void InitializePointToPoint(double f__mhz, double h_sys__meter, double N_0, int pol, double epsilon, 
     double sigma, complex<double> *Z_g, double *gamma_e, double *N_s);
-DLLEXPORT double InverseComplementaryCumulativeDistributionFunction(double q);
-DLLEXPORT double KnifeEdgeDiffraction(double d__meter, double f__mhz, double a_e__meter, double theta_los, double d_hzn__meter[2]);
-DLLEXPORT void LinearLeastSquaresFit(double pfl[], double d_start, double d_end, double *fit_y1, double *fit_y2);
-DLLEXPORT double LineOfSightLoss(double d__meter, double h_e__meter[2], complex<double> Z_g, double delta_h__meter,
+EXPORTED double InverseComplementaryCumulativeDistributionFunction(double q);
+EXPORTED double KnifeEdgeDiffraction(double d__meter, double f__mhz, double a_e__meter, double theta_los, double d_hzn__meter[2]);
+EXPORTED void LinearLeastSquaresFit(double pfl[], double d_start, double d_end, double *fit_y1, double *fit_y2);
+EXPORTED double LineOfSightLoss(double d__meter, double h_e__meter[2], complex<double> Z_g, double delta_h__meter,
     double M_d, double A_d0, double d_sML__meter, double f__mhz);
-DLLEXPORT int LongleyRice(double theta_hzn[2], double f__mhz, complex<double> Z_g, double d_hzn__meter[2], double h_e__meter[2], 
+EXPORTED int LongleyRice(double theta_hzn[2], double f__mhz, complex<double> Z_g, double d_hzn__meter[2], double h_e__meter[2], 
     double gamma_e, double N_s, double delta_h__meter, double h__meter[2], double d__meter, int mode, double *A_ref__db, 
     long *warnings, int *propmode);
-DLLEXPORT void QuickPfl(double pfl[], double gamma_e, double h__meter[2], double theta_hzn[2], double d_hzn__meter[2], 
+EXPORTED void QuickPfl(double pfl[], double gamma_e, double h__meter[2], double theta_hzn[2], double d_hzn__meter[2], 
     double h_e__meter[2], double *delta_h__meter, double *d__meter);
-DLLEXPORT double SigmaHFunction(double delta_h__meter);
-DLLEXPORT double SmoothEarthDiffraction(double d__meter, double f__mhz, double a_e__meter, double theta_los, 
+EXPORTED double SigmaHFunction(double delta_h__meter);
+EXPORTED double SmoothEarthDiffraction(double d__meter, double f__mhz, double a_e__meter, double theta_los, 
     double d_hzn__meter[2], double h_e__meter[2], complex<double> Z_g);
-DLLEXPORT double TerrainRoughness(double d__meter, double delta_h__meter);
-DLLEXPORT double TroposcatterLoss(double d__meter, double theta_hzn[2], double d_hzn__meter[2], double h_e__meter[2], 
+EXPORTED double TerrainRoughness(double d__meter, double delta_h__meter);
+EXPORTED double TroposcatterLoss(double d__meter, double theta_hzn[2], double d_hzn__meter[2], double h_e__meter[2], 
     double a_e__meter, double N_s, double f__mhz, double theta_los, double *h0);
-DLLEXPORT int ValidateInputs(double h_tx__meter, double h_rx__meter, int climate, double time,
+EXPORTED int ValidateInputs(double h_tx__meter, double h_rx__meter, int climate, double time,
     double location, double situation, double N_0, double f__mhz, int pol,
     double epsilon, double sigma, int mdvar, long *warnings);
-DLLEXPORT double Variability(double time, double location, double situation, double h_e__meter[2], double delta_h__meter,
+EXPORTED double Variability(double time, double location, double situation, double h_e__meter[2], double delta_h__meter,
     double f__mhz, double d__meter, double A_ref__db, int climate, int mdvar, long *warnings);
 
 
