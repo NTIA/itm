2ff286ef7d4c05302022b9054b3b18178f482f70
Anthony Romaniello <aromaniello@ntia.gov>
2024-09-13 11:40:58 -0400

    Add internal const to TroposcatterLoss
    Also use constexpr for Z_0, Z_1 values since they should be evaluated at compile time

--- src/TroposcatterLoss.cpp
+++ src/TroposcatterLoss.cpp
@@ -28,7 +28,7 @@ double FFunction(const double td)
     else                    // > 70 km
         i = 2;
 
-    double F_0 = a[i] + b[i] * td + c[i] * log10(td);     // [Algorithm, 6.9]
+    const double F_0 = a[i] + b[i] * td + c[i] * log10(td);  // [Algorithm, 6.9]
 
     return F_0;
 }
@@ -57,7 +57,7 @@ double TroposcatterLoss(const double d__meter, const double theta_hzn[2], const 
     double H_0;
 
     // wavenumber, k
-    double wn = f__mhz / 47.7;
+    const double wn = f__mhz / 47.7;
 
     if (*h0 > 15.0)     // short-circuit calculations if already greater than 15 dB
         H_0 = *h0;
@@ -72,11 +72,11 @@ double TroposcatterLoss(const double d__meter, const double theta_hzn[2], const 
             rr = 1.0 / rr;
         }
 
-        double theta = theta_hzn[0] + theta_hzn[1] + d__meter / a_e__meter;    // angular distance, in radians
+        const double theta = theta_hzn[0] + theta_hzn[1] + d__meter / a_e__meter;  // angular distance, in radians
 
         // [TN101, Eqn 9.4a]
-        double r_1 = 2.0 * wn * theta * h_e__meter[0];
-        double r_2 = 2.0 * wn * theta * h_e__meter[1];
+        const double r_1 = 2.0 * wn * theta * h_e__meter[0];
+        const double r_2 = 2.0 * wn * theta * h_e__meter[1];
 
         if (r_1 < 0.2 && r_2 < 0.2)
             return 1001;                // "If both r_1 and r_2 are less than 0.2 the function A_scat is not defined (or is infinite)" [Algorithm, page 11]
@@ -84,17 +84,17 @@ double TroposcatterLoss(const double d__meter, const double theta_hzn[2], const 
         double s = (d__meter - ad) / (d__meter + ad);       // asymmetry parameter
 
         // "In all of this, we truncate the values of s and q at 0.1 and 10" [Algorithm, page 16]
-        double q = MIN(MAX(0.1, rr / s), 10.0);             // TN101, Eqn 9.5
+        const double q = MIN(MAX(0.1, rr / s), 10.0);       // TN101, Eqn 9.5
         s = MAX(0.1, s);                                    // TN101, Eqn 9.5
 
         double h_0__meter = (d__meter - ad) * (d__meter + ad) * theta * 0.25 / d__meter;   // height of cross-over, [Algorithm, 4.66] [TN101v1, 9.3b]
 
-        double Z_0__meter = 1.7556e3;                       // Scale height, [Algorithm, 4.67]
-        double Z_1__meter = 8.0e3;                          // [Algorithm, 4.67]
-        double eta_s = (h_0__meter / Z_0__meter) * (1.0 + (0.031 - N_s * 2.32e-3 + pow(N_s, 2) * 5.67e-6) * exp(-pow(MIN(1.7, h_0__meter / Z_1__meter), 6)));     // Scattering efficiency factor, eta_s [TN101 Eqn 9.3a]
+        constexpr double Z_0__meter = 1.7556e3;             // Scale height, [Algorithm, 4.67]
+        constexpr double Z_1__meter = 8.0e3;                // [Algorithm, 4.67]
+        const double eta_s = (h_0__meter / Z_0__meter) * (1.0 + (0.031 - N_s * 2.32e-3 + pow(N_s, 2) * 5.67e-6) * exp(-pow(MIN(1.7, h_0__meter / Z_1__meter), 6)));  // Scattering efficiency factor, eta_s [TN101 Eqn 9.3a]
 
-        double H_00 = (H0Function(r_1, eta_s) + H0Function(r_2, eta_s)) / 2;                        // First term in TN101v1, Eqn 9.5
-        double Delta_H_0 = MIN(H_00, 6.0 * (0.6 - log10(MAX(eta_s, 1.0))) * log10(s) * log10(q));
+        const double H_00 = (H0Function(r_1, eta_s) + H0Function(r_2, eta_s)) / 2;                        // First term in TN101v1, Eqn 9.5
+        const double Delta_H_0 = MIN(H_00, 6.0 * (0.6 - log10(MAX(eta_s, 1.0))) * log10(s) * log10(q));
 
         H_0 = H_00 + Delta_H_0;                             // TN101, Eqn 9.5
         H_0 = MAX(H_0, 0.0);                                // "If Delta_H_0 would make H_0 negative, use H_0 = 0" [TN101v1, p9.4] 
@@ -108,9 +108,9 @@ double TroposcatterLoss(const double d__meter, const double theta_hzn[2], const 
     }
 
     *h0 = H_0;
-    double th = d__meter / a_e__meter - theta_los;
+    const double th = d__meter / a_e__meter - theta_los;
 
-    double D_0__meter = 40e3;   // [Algorithm, 6.8]
-    double H__meter = 47.7;     // [Algorithm, 4.63]
+    constexpr double D_0__meter = 40e3;   // [Algorithm, 6.8]
+    constexpr double H__meter = 47.7;     // [Algorithm, 4.63]
     return FFunction(th * d__meter) + 10 * log10(wn * H__meter * pow(th, 4)) - 0.1 * (N_s - 301.0) * exp(-th * d__meter / D_0__meter) + H_0;    // [Algorithm, 4.63]
 }
